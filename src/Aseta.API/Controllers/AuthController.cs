999d3bceb4ba35fc70f5591d92e7a4bf4947e55a
Vladislav Koltovich <vladislav.a.koltovicha@gmail.com>
2025-08-20 14:31:03 +0300

    fix: name controller AuthController

--- src/Aseta.API/Controllers/AuthController.cs
+++ src/Aseta.API/Controllers/AuthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Aseta.API.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/auth")]
 [ApiController]
 public class AuthController : ControllerBase
 {
